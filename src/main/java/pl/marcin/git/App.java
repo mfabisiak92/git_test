a228fa0099bd8cc0c8df9a254efcd171fb84c10e
Marcin Fabisiak <marcinfabisiak29@gmail.com>
2016-03-19 14:10:55 +0100

    initial project version

---
+++ src/main/java/pl/marcin/git/App.java
@@ -0,0 +1,14 @@
+package pl.marcin.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "To ja Marcin!" );
+    }
+}
